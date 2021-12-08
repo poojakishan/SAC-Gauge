3e462afe6b11edad0add09f037e3b43436f65ba2
poojakishan <95737232+poojakishan@users.noreply.github.com>
2021-12-08 12:13:03 +0530

    Commit Gauge.js

---
+++ Gauge.js
@@ -0,0 +1,77 @@
+import * as echarts from 'echarts';
+
+type EChartsOption = echarts.EChartsOption;
+
+var chartDom = document.getElementById('main')!;
+var myChart = echarts.init(chartDom);
+var option: EChartsOption;
+
+option = {
+  series: [
+    {
+      type: 'gauge',
+      axisLine: {
+        lineStyle: {
+          width: 30,
+          color: [
+            [0.3, '#67e0e3'],
+            [0.7, '#37a2da'],
+            [1, '#fd666d']
+          ]
+        }
+      },
+      pointer: {
+        itemStyle: {
+          color: 'auto'
+        }
+      },
+      axisTick: {
+        distance: -30,
+        length: 8,
+        lineStyle: {
+          color: '#fff',
+          width: 2
+        }
+      },
+      splitLine: {
+        distance: -30,
+        length: 30,
+        lineStyle: {
+          color: '#fff',
+          width: 4
+        }
+      },
+      axisLabel: {
+        color: 'auto',
+        distance: 40,
+        fontSize: 20
+      },
+      detail: {
+        valueAnimation: true,
+        formatter: '{value} km/h',
+        color: 'auto'
+      },
+      data: [
+        {
+          value: 70
+        }
+      ]
+    }
+  ]
+};
+
+setInterval(function () {
+  myChart.setOption<echarts.EChartsOption>({
+    series: [
+      {
+        data: [
+          {
+            value: +(Math.random() * 100).toFixed(2)
+          }
+        ]
+      }
+    ]
+  });
+}, 2000);
+
+option && myChart.setOption(option);
